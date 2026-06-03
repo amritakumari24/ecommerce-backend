978fc242661bfd8518616c59108161c47583afad
Amrita <amritakumari24@navgurukul.org>
2026-06-03 16:01:14 +0530

    added text

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const dotenv = require('dotenv');
-
 dotenv.config();
-
 const connectDB = require('./config/db');
 const logger = require("./middleware/loggerMiddleware");
 connectDB();
@@ -18,6 +16,10 @@ app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/product', require('./routes/productRoutes'));
 
 const PORT = process.env.PORT || 5000;
+
+app.get('/', (req,res)=>{
+    res.send("Hello from Backend!!!")
+})
 
 app.listen(PORT, ()=>{
     console.log(`server is running on ${PORT}`)
